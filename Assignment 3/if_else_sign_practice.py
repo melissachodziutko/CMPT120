df5e7ca7092f6d0fb5a7a8c19cb1acc5614bf0d7
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2026-02-09 18:42:16 -0500

    Create if_else_sign_practice.py

---
+++ Assignment 3/if_else_sign_practice.py
@@ -0,0 +1,12 @@
+def main():
+    '''
+    Ask the user for their test score (0–100)
+
+    If the score is 90 or above, print an A
+    If the score is 80–89, print a B
+    If the score is 70–79, print a C
+    If the score is 60–69, print a D
+    If the score is below 60, print an F
+    '''
+  
+main()
