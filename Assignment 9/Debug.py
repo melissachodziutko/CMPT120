60c90e910f6dbb86bfe339d7faac39ea65153944
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2026-04-20 13:44:17 -0400

    Create Debug.py

---
+++ Assignment 9/Debug.py
@@ -0,0 +1,11 @@
+#take the names from the text file
+#Write a short introduction for each person
+#"Hi my name is (name) and I (make up a fun fact i dont care, the more insane the better)
+
+#add your name to the text file using code :)
+
+def main():
+
+
+
+main()
