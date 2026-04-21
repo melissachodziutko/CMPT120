56fdbdd0b376dc14d2c22dcb6055f6d90248880a
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2026-04-21 12:55:16 -0400

    Create pauseandplay.py

---
+++ Assignment 9/pauseandplay.py
@@ -0,0 +1,12 @@
+#remeber the high score prompt? Yeah, so take that and do a few thingS:
+#1. make sure your high score saves to a text document called "highscore.txt"
+#2. you can still hard code a high score, BUT 
+#3. you need to check if the currently saved high score is in fact smaller than the proposed one
+#if it is, overwrite the file with the new high score
+#if it isnt, print out the current high score and challenge the user to try and beat it!
+#(again, obviously no game has to be played here)
+
+def main():
+
+
+main()
